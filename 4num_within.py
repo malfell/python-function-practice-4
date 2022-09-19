5de1e762bf54e24c3ae4cbc20ca99418517c5399
Mal F <malfellows13@gmail.com>
2022-09-19 17:14:37 -0500

    num_within() done

--- 4num_within.py
+++ 4num_within.py
@@ -4,8 +4,24 @@
 # Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, 
 # num_within(10,2,5) returns False.
 
-# 5
-# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
-# The function accepts the number n, the number of rows to print
-# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. 
-# Each number is the two numbers above it added together.
+# use begin and end numbers to list the range?
+# list(range(begin, end + 1))
+# add a plus 1 to end to make sure it's inclusive?
+
+# then check the initial number to see if it's in the list?
+
+def num_within(num, begin, end):
+    # first get the range
+    range_list = list(range(begin, end + 1))
+
+    # check if number is in the range_list
+    # if so, return true
+    if(num in range_list):
+        return True
+    # if number doesn't exist in there, return false
+    else:
+        return False
+
+print(num_within(3,2,4))
+print(num_within(3,1,3))
+print(num_within(10,2,5))
---
+++ 5pascal.py
@@ -0,0 +1,5 @@
+# 5
+# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+# The function accepts the number n, the number of rows to print
+# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. 
+# Each number is the two numbers above it added together.
