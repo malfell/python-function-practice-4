aa3c3034b50ccedc9683295e7db333525e45f800
Mal F <malfellows13@gmail.com>
2022-09-19 16:54:24 -0500

    rev_string() done

--- 3rev_string.py
+++ 3rev_string.py
@@ -1,14 +1,10 @@
 # 3
 # Write a Python function called rev_string() to reverse a string.
 
-# 4
-# Write a Python function called num_within() to check whether a number falls in a given range.
-# The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
-# Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, 
-# num_within(10,2,5) returns False.
+def rev_string(string):
+    # -1 starts at end of the string and moves backward until it reaches the 0 index
+    # revealing the initial string too
+    return f'{string}: {string[::-1]}'
 
-# 5
-# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
-# The function accepts the number n, the number of rows to print
-# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. 
-# Each number is the two numbers above it added together.
+print(rev_string('hello'))
+print(rev_string('this sentence is backwards now, wow'))
