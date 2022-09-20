4b7f0c5c5bdc9e3fff7d4b5fd6d09cba7bb86a0d
Mal F <malfellows13@gmail.com>
2022-09-20 00:19:14 -0500

    struggling

--- 5pascal.py
+++ 5pascal.py
@@ -3,3 +3,47 @@
 # The function accepts the number n, the number of rows to print
 # Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. 
 # Each number is the two numbers above it added together.
+
+# I understand what Pascal's triangle means (like the math when looking at the triangle itself),
+# but I've been struggling to understand the equation even after a lot of reading.
+# I've also been having issues after looking through the solution code
+# and other codes for writing the pascal's triangle in Python.
+# It is not clicking for me. 
+# I'm hoping to try again after more rest, but I'm turning this in for the meantime.
+
+triangle = [[1],[1,1]]
+def pascal(n):
+  #base case
+  if n < 1:
+    print("invalid number of rows")
+  elif n == 1:
+    print(triangle[0])
+  else:
+    row_number = 2
+    #fill up correct number of rows in triangle
+    while len(triangle) < n:
+      row = []
+      row_prev = triangle[row_number - 1]
+      #create correct row, then add to triangle (this row will be 1 longer than row before it)
+      length = len(row_prev)+1
+      for i in range(length):
+        #first number is 1
+        if i == 0:
+          row.append(1)
+        #intermediate numbers get added from previous rows
+        elif i > 0 and i < length-1:
+          row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
+        #last number is 1
+        else:
+          row.append(1)
+      triangle.append(row)
+      row_number += 1
+
+    #print triangle
+    for row in triangle:
+      print(row)
+
+pascal(5)
+
+
+   
